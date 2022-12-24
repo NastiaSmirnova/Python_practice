083e07b38e4c4d27866e2f742ecb8d11048feea2
SmirnovaNastasia <nastia-smy@yandex.ru>
2022-12-24 11:01:17 +0300

    fixed

--- exam/exam.py
+++ exam/exam.py
@@ -1,5 +1,5 @@
 class StrategyDeal:
-    def init(self, bank, entry, targets, close):
+    def __init__(self, bank, entry, targets, close):
         self.bank = bank
         self.entry = entry
         self.targets = targets
@@ -7,76 +7,80 @@ class StrategyDeal:
         
     def get_targets(self):
         # вернуть список таргетов в виде числовых значений float [21.5, 22.8, 23.5]
-        return targets
+        return self.targets
 
     def get_target_percents(self):
         # вернуть список процентов, как в примере, округленные до 3 знака [6.912, 13.376, 16.857]
-        return [round(it, 3) for it in ((self.targets / self.stop) - 1) * 100 ]
+        return [round(((t / self.entry) - 1) * 100, 3) for t in self.targets]
 
     def get_target_banks(self):
         # список значений банков, если продавать активы по таргетам, как в пример, округленные до 3 знака [1069.12, 1133.764, 1168.573]
-        [round(it, 3) for it in (self.targets / self.stop) * self.bank]
+        return [round(t / self.entry * self.bank, 3) for t in self.targets]
 
-    def str(self):
+    def __str__(self):
         # текстовое представление сделки
         s = f' START_PRICE: {self.entry}\n STOP_PRICE: {self.close}\n BANK: {self.bank}\n\n' 
         ts = self.get_targets()
-        ps = self.get_percents()
-        bs = self.get_banks()
+        ps = self.get_target_percents()
+        bs = self.get_target_banks()
 
         for i, t in enumerate(ts):
-          s = s + f'{i} target: {t}\n Percent: {ps[i]}\n Bank: {bs[i]}\n\n'
+          s = s + f'{i+1} target: {t}\n Percent: {ps[i]}%\n Bank: {bs[i]}\n\n'
 
         return s
 
 def read_data(file_name):
     deals = []
-    last_line = false
+    last_line = False
     bank = None
     entry = None
     targets = None
     close = None
-    with open(file_name, "r") as f:
+    with open(file_name, "r",encoding="utf-8") as f:
         for line in f.readlines():
             if line.startswith("-----"):
-                last_line = true
-                deals.add(StrategyDeal(bank, entry, targets, close))
+                last_line = True
+                deals.append(StrategyDeal(bank, entry, targets, close))
 
-        if line.startswith("BANK:"):
-            last_line = false
-            _, b = line.split()
-            bank = float(b)
+            if line.startswith("BANK:"):
+                last_line = False
+                _, b = line.split()
+                bank = float(b)
 
-        if line.startswith("Вход:"):
-            last_line = false
-            _, b = line.split()
-            entry = float(b)
+            if line.startswith("Вход:"):
+                last_line = False
+                _, b = line.split()
+                entry = float(b)
 
-        if line.target("Вход:"):
-            last_line = false
-            _, b = line.split(1)
-            targets = list(map(float, b.split(',')))
+            if line.startswith("Таргет:"):
+                last_line = False
+                _, b = line.split(" ",1)
+                targets = list(map(float, b.split(',')))
 
-        if line.startswith("Выход:"):
-            last_line = false
-            _, b = line.split()
-            close = float(b)
+            if line.startswith("Выход:"):
+                last_line = False
+                _, b = line.split()
+                close = float(b)
 
     if not last_line:
-        deals.add(StrategyDeal(bank, entry, targets, close))
+        deals.append(StrategyDeal(bank, entry, targets, close))
     return deals
 
 
 def write_data(file_name, data):
     with open(file_name, "w") as f:
+      st = True
       for d in data:
-        file.write(str(d))
+        if not st:
+          f.write("----- \n\n")
+        st = False
+        
+        f.write(str(d))
 
 
 def main():
     content = read_data('deals.txt')
     result = content
-    print(result)
     write_data('out.txt', result)
 
 
